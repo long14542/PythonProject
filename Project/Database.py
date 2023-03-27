4e8b1a3b61055391fa8462bf6f60583bae5c17f2
Võ Minh Kiệt <125539745+kiethien@users.noreply.github.com>
2023-03-27 14:55:29 +0700

    Đây là project tôi làm đến hiện tại nhé

---
+++ Project/Database.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+class Database:
+    def __init__(self):
+        self.dbConnection = sqlite3.connect("dbFile.db")
+        self.dbCursor = self.dbConnection.cursor()
+        self.dbCursor.execute("CREATE TABLE IF NOT EXISTS client_info (id PRIMARYKEY text, fName text, lName text, dob text, mob text, yob text, gender text, address text, phone text, email text, waterclock text)")
+
+    def __del__(self):
+        self.dbCursor.close()
+        self.dbConnection.close()
+
+    def Insert(self, id, fName, lName, dob, mob, yob, gender, address, phone, email,waterclock):
+        self.dbCursor.execute("INSERT INTO client_info VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (id, fName, lName, dob, mob, yob, gender, address, phone, email, waterclock))
+        self.dbConnection.commit()
+
+    def Update(self,  fName, lName, dob, mob, yob, gender, address, phone, email,waterclock,id):
+        self.dbCursor.execute("UPDATE client_info SET fName = ?, lName = ?, dob = ?, mob = ?, yob = ?, gender = ?, address = ?, phone = ?, email = ?, waterclock = ?, WHERE id = ?", (fName, lName, dob, mob, yob, gender, address, phone, email, waterclock, id))
+        self.dbConnection.commit()
+
+    def Search(self, id):
+        self.dbCursor.execute("SELECT * FROM client_info WHERE id = ?", (id, ))
+        searchResults = self.dbCursor.fetchall()
+        return searchResults
+    
+    def Delete(self, id):
+        self.dbCursor.execute("DELETE FROM client_info WHERE id = ?", (id, ))
+        self.dbConnection.commit()
+
+    def Display(self):
+        self.dbCursor.execute("SELECT * FROM client_info")
+        records = self.dbCursor.fetchall()
+        return records
---
+++ Project/DatabaseView.py
@@ -0,0 +1,47 @@
+import tkinter
+import tkinter.ttk
+import tkinter.messagebox
+
+class DatabaseView:
+    def __init__(self, data):
+        self.databaseViewWindow = tkinter.Tk()
+        self.databaseViewWindow.wm_title("Database View")
+
+        # Label widgets
+        tkinter.Label(self.databaseViewWindow, text = "Database View Window",  width = 25).grid(pady = 5, column = 1, row = 1)
+
+        self.databaseView = tkinter.ttk.Treeview(self.databaseViewWindow)
+        self.databaseView.grid(pady = 5, column = 1, row = 2)
+        self.databaseView["show"] = "headings"
+        self.databaseView["columns"] = ("id", "fName", "lName", "dob", "mob", "yob", "gender", "address", "phone", "email",  "water clock")
+
+        # Treeview column headings
+        self.databaseView.heading("id", text = "ID")
+        self.databaseView.heading("fName", text = "First Name")
+        self.databaseView.heading("lName", text = "Last Name")
+        self.databaseView.heading("dob", text = "D.O.B")
+        self.databaseView.heading("mob", text = "M.O.B")
+        self.databaseView.heading("yob", text = "Y.O.B")
+        self.databaseView.heading("gender", text = "Gender")
+        self.databaseView.heading("address", text = "Home Address")
+        self.databaseView.heading("phone", text = "Phone Number")
+        self.databaseView.heading("email", text = "Email ID")
+        self.databaseView.heading("water clock", text= "Water Clock")
+
+        # Treeview columns
+        self.databaseView.column("id", width = 40)
+        self.databaseView.column("fName", width = 100)
+        self.databaseView.column("lName", width = 100)
+        self.databaseView.column("dob", width = 60)
+        self.databaseView.column("mob", width = 60)
+        self.databaseView.column("yob", width = 60)
+        self.databaseView.column("gender", width = 60)
+        self.databaseView.column("address", width = 200)
+        self.databaseView.column("phone", width = 100)
+        self.databaseView.column("email", width = 200)
+        self.databaseView.column("water clock", width=200)
+
+        for record in data:
+            self.databaseView.insert('', 'end', values=(record))
+
+        self.databaseViewWindow.mainloop()

da7d99b4145a53046bfcca50946b9c1b35d01243
long14542 <longphanduy118@gmail.com>
2023-04-17 00:08:41 +0700

    Update SearchWindow.py

--- SearchWindow.py
+++ SearchWindow.py
@@ -1,35 +1,34 @@
-import tkinter as tk
-import tkinter.ttk as ttk
-import tkinter.messagebox as messagebox
+import tkinter
+import tkinter.ttk
+import tkinter.messagebox
 from Database import Database
 from DatabaseView import DatabaseView
 
-class SearchDeleteWindow:
-    def __init__(self, task):
-        window = tk.Tk()
-        window.wm_title(task + " data")
+class SearchWindow:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.wm_title("Search data")
+        self.window.geometry("500x200+240+100")
+        self.window.iconbitmap("D:\B2\PythonProject\icon\SearchIcon.ico")
+        self.window.wm_resizable(False,False)
 
-        self.id = tk.StringVar()
-        self.fName = tk.StringVar()
-        self.lName = tk.StringVar()
-        self.heading = "Please enter client ID to " + task
+        # Initializing all the variables
+        self.id = tkinter.StringVar()
+        self.heading = "Please enter client ID to search "
 
-        tk.Label(window, text=self.heading, width=50).grid(pady=20, row=1)
-        tk.Label(window, text="client ID", width=10).grid(pady=5, row=2)
+        # Labels
+        tkinter.Label(self.window, text = self.heading, width = 50).grid(pady = 20, row = 1)
+        tkinter.Label(self.window, text = "client ID", width = 10).grid(pady = 5,column=0, row = 2)
 
-        self.idEntry = tk.Entry(window, width=5, textvariable=self.id)
-        self.idEntry.grid(pady=5, row=3)
+        # Entry widgets
+        self.idEntry = tkinter.Entry(self.window, width = 5, textvariable = self.id)
+        self.idEntry.grid(pady = 5, row = 3)
 
-        if task == "Search":
-            tk.Button(window, width=20, text=task, command=self.Search).grid(pady=15, padx=5, column=1, row=14)
-        elif task == "Delete":
-            tk.Button(window, width=20, text=task, command=self.Delete).grid(pady=15, padx=5, column=1, row=14)
+        # Button widgets
+        tkinter.Button(self.window, width = 10, text = "Search", command = self.Search).place(x=280, y=150)
+        tkinter.Button(self.window, width = 10, text = "Close", command = self.window.destroy).place(x=370, y=150)
 
     def Search(self):
         self.database = Database()
-        self.data = self.database.Search(self.idEntry.get())
+        self.data = self.database.Search2(self.idEntry.get())
         self.databaseView = DatabaseView(self.data)
-
-    def Delete(self):
-        self.database = Database()
-        self.database.Delete(self.idEntry.get())
